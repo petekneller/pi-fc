cf062e20554e6ddb838524bc256a8778b8c90774
Pete Kneller <mrkneller@hotmail.com>
2017-02-22 16:46:07 +0000

    Stop trying to change SPI mode during test - seems its not legal to change it on the RPi. Instead try word-size

--- scala/spidev/src/test/scala/spidev/IOCtlTest.scala
+++ scala/spidev/src/test/scala/spidev/IOCtlTest.scala
@@ -12,28 +12,26 @@ import Spidev._
 
 class IOCtlTest extends FlatSpec with Matchers with TypeCheckedTripleEquals {
 
-  "ioctl" should "support get/set of SPI mode" in {
+  "ioctl" should "support get/set of SPI word size" in {
     assert(new File("/dev/spidev0.0").exists)
 
     val fd = open("/dev/spidev0.0", O_RDWR)
     val data1 = ByteBuffer.allocate(Native.getNativeSize(classOf[Byte]))
     data1.order(LITTLE_ENDIAN)
-    ioctl(fd, new NativeLong(SPI_IOC_RD_MODE.unsigned), data1)
-
-    val existingMode = data1.get
-    existingMode should === (SPI_MODE_0)
+    ioctl(fd, new NativeLong(SPI_IOC_RD_BITS_PER_WORD.unsigned), data1)
+    data1.get should === (8.toByte)
 
     val data2 = ByteBuffer.allocate(Native.getNativeSize(classOf[Byte]))
     data2.order(LITTLE_ENDIAN)
-    data2.put(SPI_MODE_2)
-    ioctl(fd, new NativeLong(SPI_IOC_WR_MODE.unsigned), data2)
+    data2.put(16.toByte)
+    ioctl(fd, new NativeLong(SPI_IOC_WR_BITS_PER_WORD.unsigned), data2)
 
     val data3 = ByteBuffer.allocate(Native.getNativeSize(classOf[Byte]))
     data3.order(LITTLE_ENDIAN)
-    ioctl(fd, new NativeLong(SPI_IOC_RD_MODE.unsigned), data3)
-    data3.get should === (SPI_MODE_2)
+    ioctl(fd, new NativeLong(SPI_IOC_RD_BITS_PER_WORD.unsigned), data3)
+    data3.get should === (16.toByte)
 
-    ioctl(fd, new NativeLong(SPI_IOC_WR_MODE.unsigned), data1)
+    ioctl(fd, new NativeLong(SPI_IOC_WR_BITS_PER_WORD.unsigned), data1)
   }
 
 }
