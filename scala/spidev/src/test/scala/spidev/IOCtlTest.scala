77aa7f5e937f6758d48cbd6fd3bf795701950fd7
Pete Kneller <mrkneller@hotmail.com>
2017-02-23 12:23:28 +0000

    Revise tests so that most SPI commands are only tested in 'read' and the clock speed is the only one tested as a write op

--- scala/spidev/src/test/scala/spidev/IOCtlTest.scala
+++ scala/spidev/src/test/scala/spidev/IOCtlTest.scala
@@ -12,26 +12,61 @@ import Spidev._
 
 class IOCtlTest extends FlatSpec with Matchers with TypeCheckedTripleEquals {
 
-  "ioctl" should "support get/set of SPI word size" in {
-    assert(new File("/dev/spidev0.0").exists)
-
-    val fd = open("/dev/spidev0.0", O_RDWR)
-    val data1 = ByteBuffer.allocate(Native.getNativeSize(classOf[Byte]))
-    data1.order(LITTLE_ENDIAN)
-    ioctl(fd, new NativeLong(SPI_IOC_RD_BITS_PER_WORD.unsigned), data1)
-    data1.get should === (8.toByte)
-
-    val data2 = ByteBuffer.allocate(Native.getNativeSize(classOf[Byte]))
-    data2.order(LITTLE_ENDIAN)
-    data2.put(16.toByte)
-    ioctl(fd, new NativeLong(SPI_IOC_WR_BITS_PER_WORD.unsigned), data2)
-
-    val data3 = ByteBuffer.allocate(Native.getNativeSize(classOf[Byte]))
-    data3.order(LITTLE_ENDIAN)
-    ioctl(fd, new NativeLong(SPI_IOC_RD_BITS_PER_WORD.unsigned), data3)
-    data3.get should === (16.toByte)
-
-    ioctl(fd, new NativeLong(SPI_IOC_WR_BITS_PER_WORD.unsigned), data1)
+  "ioctl" should "support fetch of SPI mode (8 bit value)" in {
+    fetchCommandValue(SPI_IOC_RD_MODE, classOf[Byte]).get should === (SPI_MODE_0)
   }
+
+  "ioctl" should "support fetch of SPI mode (32 bit value)" in {
+    fetchCommandValue(SPI_IOC_RD_MODE32, classOf[Int]).asIntBuffer.get should === (0)
+  }
+
+  it should "support fetch of SPI bit justification" in {
+    fetchCommandValue(SPI_IOC_RD_LSB_FIRST, classOf[Byte]).get should === (SPI_LSB_FIRST)
+  }
+
+  it should "support fetch of SPI word size" in {
+    fetchCommandValue(SPI_IOC_RD_BITS_PER_WORD, classOf[Byte]).get should === (8.toByte)
+  }
+
+  it should "support get/set of SPI clock speed" in {
+    assert(new File(device0).exists)
+
+    val fd = open(device0, O_RDWR)
+    val data = ByteBuffer.allocate(Native.getNativeSize(classOf[Int]))
+    data.order(LITTLE_ENDIAN)
+    ioctl(fd, new NativeLong(SPI_IOC_RD_MAX_SPEED_HZ.unsigned), data)
+    val originalSpeed = data.asIntBuffer.get
+    originalSpeed should be > (0)
+
+    data.clear()
+    data.asIntBuffer.put(0, 250000)
+    ioctl(fd, new NativeLong(SPI_IOC_WR_MAX_SPEED_HZ.unsigned), data)
+
+    data.clear()
+    data.asIntBuffer.put(0, 0)
+    ioctl(fd, new NativeLong(SPI_IOC_RD_MAX_SPEED_HZ.unsigned), data)
+    data.asIntBuffer.get should === (250000)
+
+    data.clear()
+    data.asIntBuffer.put(0, originalSpeed)
+    ioctl(fd, new NativeLong(SPI_IOC_WR_MAX_SPEED_HZ.unsigned), data)
+  }
+
+
+  def fetchCommandValue[A](command: Int, argumentSize: Class[A]): ByteBuffer = {
+    assert(new File(device0).exists)
+
+    val fd = open(device0, O_RDONLY)
+    try {
+      val data = ByteBuffer.allocate(Native.getNativeSize(argumentSize))
+      data.order(LITTLE_ENDIAN)
+      ioctl(fd, new NativeLong(command.unsigned), data)
+      data
+    } finally {
+      close(fd)
+    }
+  }
+
+  val device0 = "/dev/spidev0.0"
 
 }
