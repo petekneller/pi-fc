e522be1309d60079282ae565c840c26ea4571ef9
Pete Kneller <mrkneller@hotmail.com>
2017-03-01 20:51:22 +0000

    Tidy up MPU9250 with recent changes in device API and what I've learnt about using the type aliases in Address and Controller

--- scala/fc/src/main/scala/fc/device/sensor/Mpu9250.scala
+++ scala/fc/src/main/scala/fc/device/sensor/Mpu9250.scala
@@ -7,44 +7,34 @@ import ioctl.syntax._
 import fc.{device => d}
 import d._
 
-trait Mpu9250 {
-  val address: Address
-  implicit val controller: Controller { type Bus = address.Bus }
+trait Mpu9250 extends Device {
+  import Mpu9250.{registers, constants}
 
-  import Mpu9250._
+  def checkCommunication(): Either[DeviceException, Boolean] = Rx.byte(registers.WHOAMI).read(address).map(_ === constants.DEVICE_ID)
 
-  def checkCommunication(): Either[DeviceException, Unit] = readRegister(registers.WHOAMI).map(_ === constants.DEVICE_ID)
-
-  def readGyro(): Either[DeviceException, (Double, Double, Double)] = for {
-    x <- readRegisterPair(registers.GYRO_XOUT_L, registers.GYRO_XOUT_H)
-    y <- readRegisterPair(registers.GYRO_YOUT_L, registers.GYRO_YOUT_H)
-    z <- readRegisterPair(registers.GYRO_ZOUT_L, registers.GYRO_ZOUT_H)
-  } yield (x.toDouble, y.toDouble, z.toDouble)
-
-  private def readRegister(register: Register) = Rx.byte(register).read(address)
-
-  private def readRegisterPair(loRegister: Register, hiRegister: Register): Either[DeviceException, Short] = for {
-    hiByte <- readRegister(hiRegister)
-    loByte <- readRegister(loRegister)
-  } yield ((hiByte.unsigned << 8) | loByte).toShort
+  def readGyro(): Either[DeviceException, (Short, Short, Short)] = for {
+    x <- Rx.short(registers.GYRO_XOUT_L, registers.GYRO_XOUT_H).read(address)
+    y <- Rx.short(registers.GYRO_YOUT_L, registers.GYRO_YOUT_H).read(address)
+    z <- Rx.short(registers.GYRO_ZOUT_L, registers.GYRO_ZOUT_H).read(address)
+  } yield (x, y, z)
 
 }
 
 object Mpu9250 {
 
-  def apply[A](a: Address { type Bus = A })(implicit c: Controller { type Bus = A }) = new Mpu9250 {
-    val address = a
-    implicit val controller = c
+  def apply(a: Address)(implicit c: Controller { type Bus = a.Bus }) = new Mpu9250 {
+    val address: Address { type Bus = a.Bus } = a
+    implicit val controller: Controller { type Bus = address.Bus } = c
   }
 
   object registers {
-    val WHOAMI = Register(0x75)
     val GYRO_XOUT_H = Register(0x43)
     val GYRO_XOUT_L = Register(0x44)
     val GYRO_YOUT_H = Register(0x45)
     val GYRO_YOUT_L = Register(0x46)
     val GYRO_ZOUT_H = Register(0x47)
     val GYRO_ZOUT_L = Register(0x48)
+    val WHOAMI =      Register(0x75)
   }
 
   object constants {
