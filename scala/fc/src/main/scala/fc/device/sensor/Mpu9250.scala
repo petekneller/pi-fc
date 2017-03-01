b3f07ebf7e48154ab9bc122083d1cad73660263f
Pete Kneller <mrkneller@hotmail.com>
2017-03-01 21:49:32 +0000

    Add sleep and reset functions

--- scala/fc/src/main/scala/fc/device/sensor/Mpu9250.scala
+++ scala/fc/src/main/scala/fc/device/sensor/Mpu9250.scala
@@ -4,13 +4,18 @@ import cats.syntax.either._
 import cats.syntax.eq._
 import cats.instances.byte._
 import ioctl.syntax._
-import fc.{device => d}
-import d._
+import fc.device._
+import fc.device.configuration._
 
 trait Mpu9250 extends Device {
-  import Mpu9250.{registers, constants}
+  import Mpu9250.{registers, constants, config}
 
   def checkCommunication(): Either[DeviceException, Boolean] = Rx.byte(registers.WHOAMI).read(address).map(_ === constants.DEVICE_ID)
+
+  def enable(value: Boolean = true): Either[DeviceException, Unit] = config.SLEEP.write(address, !value)
+  def disable(): Either[DeviceException, Unit] = enable(false)
+
+  def reset(): Either[DeviceException, Unit] = config.H_RESET.write(address, true)
 
   def readGyro(): Either[DeviceException, (Short, Short, Short)] = for {
     x <- Rx.short(registers.GYRO_XOUT_L, registers.GYRO_XOUT_H).read(address)
@@ -27,6 +32,15 @@ object Mpu9250 {
     implicit val controller: Controller { type Bus = address.Bus } = c
   }
 
+  object config {
+    val SLEEP =   BitFlagConfiguration(registers.PWR_MGMT_1, 6)
+    val H_RESET = BitFlagConfiguration(registers.PWR_MGMT_1, 7) // hardware reset - all config registers go to their default values
+  }
+
+  object constants {
+    val DEVICE_ID: Byte = 0x71
+  }
+
   object registers {
     val GYRO_XOUT_H = Register(0x43)
     val GYRO_XOUT_L = Register(0x44)
@@ -34,11 +48,8 @@ object Mpu9250 {
     val GYRO_YOUT_L = Register(0x46)
     val GYRO_ZOUT_H = Register(0x47)
     val GYRO_ZOUT_L = Register(0x48)
+    val PWR_MGMT_1 =  Register(0x6B)
     val WHOAMI =      Register(0x75)
-  }
-
-  object constants {
-    val DEVICE_ID: Byte = 0x71
   }
 
 }
