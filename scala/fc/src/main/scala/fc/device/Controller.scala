527f46be432dd17ad11df51d08592cef6d981801
Pete Kneller <mrkneller@hotmail.com>
2017-03-01 14:32:33 +0000

    Add a little docs on which use cases the device exceptions were designed for.

--- scala/fc/src/main/scala/fc/device/Controller.scala
+++ scala/fc/src/main/scala/fc/device/Controller.scala
@@ -9,8 +9,17 @@ package fc.device
 trait Controller { self =>
   type Bus
 
+  /*
+   @throws DeviceUnavailableException should something occur while trying to initially connect to the device
+   @throws IncompleteDataException if the device failed to provide the requested number of bytes
+   @throws TransferFailedException to signal a general low-level failure within the transer
+   */
   def receive(device: Address { type Bus = self.Bus }, register: Register, numBytes: Int): Either[DeviceException, Seq[Byte]]
 
+  /*
+   @throws DeviceUnavailableException should something occur while trying to initially connect to the device
+   @throws TransferFailedException to signal a general low-level failure within the transer
+   */
   def transmit(device: Address { type Bus = self.Bus }, register: Register, data: Byte): Either[DeviceException, Unit]
 }
 
