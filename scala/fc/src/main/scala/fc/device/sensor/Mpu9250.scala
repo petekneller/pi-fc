897e994764c5cc00d9435a0358cf0dd6ce750c86
Pete Kneller <mrkneller@hotmail.com>
2017-03-03 11:16:08 +0000

    Add accelerometer config/measurement to the MPU9250

--- scala/fc/src/main/scala/fc/device/sensor/Mpu9250.scala
+++ scala/fc/src/main/scala/fc/device/sensor/Mpu9250.scala
@@ -26,6 +26,14 @@ trait Mpu9250 extends Device {
       fullScale
     ).read(address)
 
+  def readAccel(fullScale: enums.AccelFullScale.Val): Either[DeviceException, (Double, Double, Double)] =
+    Measurement(
+      registers.ACCEL_XOUT_L, registers.ACCEL_XOUT_H,
+      registers.ACCEL_YOUT_L, registers.ACCEL_YOUT_H,
+      registers.ACCEL_ZOUT_L, registers.ACCEL_ZOUT_H,
+      fullScale
+    ).read(address)
+
 }
 
 object Mpu9250 {
@@ -36,9 +44,10 @@ object Mpu9250 {
   }
 
   object config {
-    val GYRO_FS_SEL = MultiBitFlag(registers.GYRO_CONFIG, 4, 2, enums.GyroFullScale)
-    val SLEEP =       SingleBitFlag(registers.PWR_MGMT_1, 6)
-    val H_RESET =     SingleBitFlag(registers.PWR_MGMT_1, 7) // hardware reset - all config registers go to their default values
+    val GYRO_FS_SEL =    MultiBitFlag(registers.GYRO_CONFIG, 4, 2, enums.GyroFullScale)
+    val ACCEL_FS_SEL =   MultiBitFlag(registers.ACCEL_CONFIG_1, 4, 2, enums.AccelFullScale)
+    val SLEEP =          SingleBitFlag(registers.PWR_MGMT_1, 6)
+    val H_RESET =        SingleBitFlag(registers.PWR_MGMT_1, 7) // hardware reset - all config registers go to their default values
   }
 
   object constants {
@@ -47,15 +56,22 @@ object Mpu9250 {
 
   object registers {
 
-    val GYRO_CONFIG = Register(27)
-    val GYRO_XOUT_H = Register(67)
-    val GYRO_XOUT_L = Register(68)
-    val GYRO_YOUT_H = Register(69)
-    val GYRO_YOUT_L = Register(70)
-    val GYRO_ZOUT_H = Register(71)
-    val GYRO_ZOUT_L = Register(72)
-    val PWR_MGMT_1 =  Register(107)
-    val WHOAMI =      Register(117)
+    val GYRO_CONFIG =      Register(27)
+    val ACCEL_CONFIG_1 =   Register(28)
+    val ACCEL_XOUT_H =     Register(59)
+    val ACCEL_XOUT_L =     Register(60)
+    val ACCEL_YOUT_H =     Register(61)
+    val ACCEL_YOUT_L =     Register(62)
+    val ACCEL_ZOUT_H =     Register(63)
+    val ACCEL_ZOUT_L =     Register(64)
+    val GYRO_XOUT_H =      Register(67)
+    val GYRO_XOUT_L =      Register(68)
+    val GYRO_YOUT_H =      Register(69)
+    val GYRO_YOUT_L =      Register(70)
+    val GYRO_ZOUT_H =      Register(71)
+    val GYRO_ZOUT_L =      Register(72)
+    val PWR_MGMT_1 =       Register(107)
+    val WHOAMI =           Register(117)
 
   } // registers
 
@@ -71,6 +87,18 @@ object Mpu9250 {
       object dps2000 extends Val { val value = 0x3.toByte; val factor = 2000.0 }
 
       def values = Set(dps250, dps500, dps1000, dps2000)
+    }
+
+    object AccelFullScale extends FlagEnumeration {
+      type T = Val
+
+      sealed trait Val extends Flag with FullScale
+      object g2 extends Val { val value = 0x0.toByte; val factor = 2.0 }
+      object g4 extends Val { val value = 0x1.toByte; val factor = 4.0 }
+      object g8 extends Val { val value = 0x2.toByte; val factor = 8.0 }
+      object g16 extends Val { val value = 0x3.toByte; val factor = 16.0 }
+
+      def values = Set(g2, g4, g8, g16)
     }
 
   } // enums
