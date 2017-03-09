f7f224c5672f154095cf5b2461df5d93c82d0da1
Pete Kneller <mrkneller@hotmail.com>
2017-03-09 11:06:13 +0000

    Update ESC with new knowledge of how the PWM controller works on the Navio2

--- scala/fc/src/main/scala/fc/device/output/ESC.scala
+++ scala/fc/src/main/scala/fc/device/output/ESC.scala
@@ -3,18 +3,24 @@ package fc.device.output
 import cats.syntax.either._
 import fc.device._
 
-case class ESC(pwmChannel: PwmChannel) {
+case class ESC(
+  pwmChannel: PwmChannel,
+  armPulseMicroseconds: Long = 900L,
+  disarmPulseMicroseconds: Long = 0L,
+  minPulseMicroseconds: Long = 1100L,
+  maxPulseMicroseconds: Long = 1900L
+) {
 
   def init(): DeviceResult[Unit] = for {
-    _ <- disable()
+    _ <- disarm()
     _ <- pwmChannel.write(PwmChannel.configs.frequencyHz)(50)
-    _ <- setPulseWidthMicroseconds(900) // below 1000 us is a safe 'off' throttle for most ESCs
   } yield ()
 
-  def enable(value: Boolean = true): DeviceResult[Unit] = pwmChannel.write(PwmChannel.configs.enable)(value)
-  def disable(): DeviceResult[Unit] = enable(false)
+  def arm(arming: Boolean = true): DeviceResult[Long] = setPulseWidthMicroseconds(if (arming) armPulseMicroseconds else disarmPulseMicroseconds)
+  def disarm(): DeviceResult[Long] = arm(false)
 
-  def getPulseWidthMicroseconds(): DeviceResult[Long] = pwmChannel.read(PwmChannel.configs.pulseWidthNanoseconds).map(microseconds => (microseconds / 1e3).round)
-
-  def setPulseWidthMicroseconds(pulseWidth: Long): DeviceResult[Unit] = pwmChannel.write(PwmChannel.configs.pulseWidthNanoseconds)((pulseWidth * 1e9).round)
+  def setPulseWidthMicroseconds(pulseWidth: Long): DeviceResult[Long] = {
+    val pulseToSet = pulseWidth.max(minPulseMicroseconds).min(maxPulseMicroseconds)
+    pwmChannel.write(PwmChannel.configs.pulseWidthNanoseconds)((pulseWidth * 1e9).round) map (_ => pulseToSet)
+  }
 }
