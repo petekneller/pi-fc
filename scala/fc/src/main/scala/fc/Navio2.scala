2bf9a734b52a0bbfab6ced5495da0c091aca4d7c
Pete Kneller <mrkneller@hotmail.com>
2017-03-16 16:54:26 +0000

    Pull out a useful value in the Navio config

--- scala/fc/src/main/scala/fc/Navio2.scala
+++ scala/fc/src/main/scala/fc/Navio2.scala
@@ -14,7 +14,10 @@ object Navio2 {
 
   val receiver = RcReceiver()
 
-  val esc1 = ESC(PwmChannel(chipNumber = 0, channelNumber = 1)) // pin 2
+  // It's convenient to have low-level access to an ESC for testing configuration
+  val pwmChannel1 = PwmChannel(chipNumber = 0, channelNumber = 1)
+
+  val esc1 = ESC(pwmChannel1) // pin 2
   val esc2 = ESC(PwmChannel(chipNumber = 0, channelNumber = 3)) // pin 4
   val esc3 = ESC(PwmChannel(chipNumber = 0, channelNumber = 5)) // pin 6
   val esc4 = ESC(PwmChannel(chipNumber = 0, channelNumber = 7)) // pin 8
