24dbeb845206f97c93c54ffdb190cab13157747e
Pete Kneller <mrkneller@hotmail.com>
2017-03-18 15:14:26 +0000

    Add fs2 task to display raw RC inputs to the console

--- scala/fc/src/main/scala/fc/Navio2.scala
+++ scala/fc/src/main/scala/fc/Navio2.scala
@@ -1,5 +1,6 @@
 package fc
 
+import cats.syntax.either._
 import device.spi.{SpiController, SpiAddress}
 import device.file.FileController
 import device.rc.RcAddress
@@ -39,5 +40,16 @@ object Navio2 {
   /* Tasks */
 
   def motorsTest = tasks.motorsTest(escs.one, escs.two, escs.three, escs.four) to tasks.printToConsole
+
+  def displayRcChannels =
+    tasks.zip5(tasks.readChannel(receiver, rcChannels.one),
+               tasks.readChannel(receiver, rcChannels.two),
+               tasks.readChannel(receiver, rcChannels.three),
+               tasks.readChannel(receiver, rcChannels.four),
+               tasks.readChannel(receiver, rcChannels.six)) map ( dr => dr.map {
+    case (one, two, three, four, six) =>
+      val fmt = "CH %d: [%4d]"
+      (fmt.format(1, one.ppm) :: fmt.format(2, two.ppm) :: fmt.format(3, three.ppm) :: fmt.format(4, four.ppm) :: fmt.format(6, six.ppm) :: Nil).mkString(" | ")
+  }) to tasks.printToConsole
 
 }
--- scala/fc/src/main/scala/fc/task/fs2/package.scala
+++ scala/fc/src/main/scala/fc/task/fs2/package.scala
@@ -4,15 +4,16 @@ import cats.syntax.either._
 import _root_.fs2.{Stream, Task, Sink}
 import squants.time.{Time, Seconds}
 import fc.device.DeviceResult
+import fc.device.rc.RcInput
+import fc.device.input.{RcReceiver, RcChannel}
 import fc.device.output.ESC
 
 package object fs2 {
-
   def motorArm(esc: ESC, arm: Boolean): Stream[Task, DeviceResult[Int]] = Stream.eval(Task.delay{ esc.arm(arm) })
 
   def motorRun(esc: ESC, throttle: Double): Stream[Task, DeviceResult[Int]] = Stream.eval(Task.delay{ esc.run(throttle) })
 
-  def sleep(period: Time): Stream[Task, Nothing] = Stream.eval(Task.delay{ Thread.sleep(period.toMilliseconds.toInt) }).flatMap(_ => Stream.empty)
+  def sleep(period: Time): Stream[Task, Nothing] = Stream.eval(Task.delay{ Thread.sleep(period.toMilliseconds.toLong) }).flatMap(_ => Stream.empty)
 
   def motorTest(esc: ESC): Stream[Task, String] = {
     def sleep = fs2.sleep(Seconds(0.5)).map(_ => s"sleep for 0.5 seconds")
@@ -30,5 +31,18 @@ package object fs2 {
     escs.foldLeft(Stream.empty[Task, String])((stream, esc) => stream ++ motorTest(esc))
 
   def printToConsole[A]: Sink[Task, A] = s => s.flatMap(a => Stream.eval(Task.delay{ println(a.toString) }))
+
+  def zip5[A, B, C, D, E](a: Stream[Task, DeviceResult[A]], b: Stream[Task, DeviceResult[B]], c: Stream[Task, DeviceResult[C]], d: Stream[Task, DeviceResult[D]], e: Stream[Task, DeviceResult[E]]) =
+    (a zip b zip c zip d zip e) map { case ((((aDR, bDR), cDR), dDR), eDR) =>
+      for {
+        a <- aDR
+        b <- bDR
+        c <- cDR
+        d <- dDR
+        e <- eDR
+      } yield (a, b, c, d, e)
+    }
+
+  def readChannel(receiver: RcReceiver, channel: RcChannel): Stream[Task, DeviceResult[RcInput]] = Stream.eval(Task.delay{ receiver.readChannel(channel) })
 
 }
