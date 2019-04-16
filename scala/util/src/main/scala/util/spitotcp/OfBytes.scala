94d676c98a19923cf76ceae53602920dcbe3364e
Pete Kneller <mrkneller@hotmail.com>
2019-04-16 21:57:46 +0100

    Adds the first metric to the Spi to Tcp util
    The first metric prints to stdout the number of bytes received from the
    client socket on each fetch.

--- scala/util/src/main/scala/util/spitotcp/OfBytes.scala
+++ scala/util/src/main/scala/util/spitotcp/OfBytes.scala
@@ -11,6 +11,7 @@ import eu.timepit.refined.numeric.Interval
 import eu.timepit.refined.auto.{ autoRefineV, autoUnwrap }
 import cats.effect.{ IO, Timer }
 import fs2.{ Stream, Pipe, Chunk }
+import fs2.concurrent.SignallingRef
 import fs2.io.tcp.Socket
 import v7.SpiToTcp.{ spiTransfer, spiReceive, delay, receiveFromClient, transmitToClient }
 
@@ -24,25 +25,38 @@ object OfBytes {
   def apply(port: Port): Unit = {
 
     implicit val acg = AsynchronousChannelGroup.withThreadPool(Executors.newCachedThreadPool())
-    val mainStream = for {
+    val app = for {
       clientResource <- Socket.server[IO](new InetSocketAddress("0.0.0.0", port))
       clientSocket <- Stream.resource(clientResource)
-      _ <- handlePeer(clientSocket)
+      receiveMetric <- Stream.eval(SignallingRef[IO, Int](0))
+      tcpStream = handlePeer(clientSocket, receiveMetric)
+      metricStream = printMetrics(receiveMetric)
+      _ <- Stream(tcpStream, metricStream).parJoinUnbounded
     } yield ()
 
-    mainStream.compile.drain.unsafeRunSync()
+    app.compile.drain.unsafeRunSync()
   }
 
-  def handlePeer(client: Socket[IO]): Stream[IO, Unit] = {
+  def handlePeer(client: Socket[IO], receiveMetric: SignallingRef[IO, Int]): Stream[IO, Unit] = {
     val ping = Stream.awakeEvery[IO](delay)
 
+    val chunksFromClient = receiveFromClient(client).chunks.map(_.toArray: Seq[Byte])
+    val withReceiveMetric = for {
+      bytes <-chunksFromClient
+      _ <- Stream.eval(receiveMetric.set(bytes.length))
+    } yield bytes
+
     val transferViaSpi: Pipe[IO, Either[Seq[Byte], FiniteDuration], Byte] = upstream => {
-      upstream flatMap { 
+      upstream flatMap {
         case Left(clientBytes) => Stream.eval(cs.evalOn(blockingIO)(spiTransfer(clientBytes)))
         case Right(_) => Stream.eval(cs.evalOn(blockingIO)(spiReceive()))
       } flatMap { bytes => Stream.chunk(Chunk.seq(bytes))}
     }
 
-    (receiveFromClient(client).chunks.map(_.toArray: Seq[Byte]) either ping) through transferViaSpi through transmitToClient(client)
+    (withReceiveMetric either ping) through transferViaSpi through transmitToClient(client)
+  }
+
+  def printMetrics(receiveMetric: SignallingRef[IO, Int]): Stream[IO, Unit] = {
+    receiveMetric.discrete.flatMap(receiveBytes => Stream.eval(cs.evalOn(blockingIO)(IO.delay{ println(s"Received $receiveBytes bytes") })))
   }
 }
