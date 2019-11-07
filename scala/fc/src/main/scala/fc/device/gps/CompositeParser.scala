b321c8473f7ea4ccc3eec93b1f69ba640594cec4
Pete Kneller <mrkneller@hotmail.com>
2019-11-07 13:45:50 +0000

    Reorganises and tidies the composite message parser

--- scala/fc/src/main/scala/fc/device/gps/CompositeParser.scala
+++ scala/fc/src/main/scala/fc/device/gps/CompositeParser.scala
@@ -2,38 +2,49 @@ package fc.device.gps
 
 import MessageParser._
 
-class CompositeParser[A <: Message, B <: Message](a: MessageParser[A], b: MessageParser[B]) extends MessageParser[CompositeMessage[A, B]] {
-  def consume(byte: Byte): ParseState[CompositeMessage[A, B]] = a.consume(byte) match {
-    case Unconsumed(_) => b.consume(byte) match {
-      case Unconsumed(bytes) => Unconsumed[CompositeMessage[A, B]](bytes)
-      case Proceeding(next) => Proceeding(new WrapperB(next))
-      case Done(msg) => Done(Right(msg))
-      case Failed(cause) => Failed[CompositeMessage[A, B]](cause)
-    }
-    case Proceeding(next) => Proceeding(new WrapperA(next))
-    case Done(msg) => Done(Left(msg))
-    case Failed(cause) => Failed[CompositeMessage[A, B]](cause)
-  }
-}
-
-class WrapperA[A <: Message, B <: Message](delegate: MessageParser[A]) extends MessageParser[CompositeMessage[A, B]] {
-  def consume(byte: Byte): ParseState[CompositeMessage[A, B]] = delegate.consume(byte) match {
-    case Unconsumed(bytes) => Unconsumed[CompositeMessage[A, B]](bytes)
-    case Proceeding(next) => Proceeding(new WrapperA(next))
-    case Done(msg) => Done(Left(msg))
-    case Failed(cause) => Failed[CompositeMessage[A, B]](cause)
-  }
-}
-
-class WrapperB[A <: Message, B <: Message](delegate: MessageParser[B]) extends MessageParser[CompositeMessage[A, B]] {
-  def consume(byte: Byte): ParseState[CompositeMessage[A, B]] = delegate.consume(byte) match {
-    case Unconsumed(bytes) => Unconsumed[CompositeMessage[A, B]](bytes)
-    case Proceeding(next) => Proceeding(new WrapperB(next))
-    case Done(msg) => Done(Right(msg))
-    case Failed(cause) => Failed[CompositeMessage[A, B]](cause)
-  }
-}
-
 sealed trait CompositeMessage[A <: Message, B <: Message] extends Message
 case class Left[A <: Message, B <: Message](msg: A) extends CompositeMessage[A, B]
 case class Right[A <: Message, B <: Message](msg: B) extends CompositeMessage[A, B]
+
+object CompositeParser {
+  def apply[A <: Message, B <: Message](a: MessageParser[A], b: MessageParser[B]): CompositeParser[A, B] =
+    new CompositeParser(a, b)
+
+  private class WrapperA[A <: Message, B <: Message](delegate: MessageParser[A]) extends MessageParser[CompositeMessage[A, B]] {
+    def consume(byte: Byte): ParseState[CompositeMessage[A, B]] = delegate.consume(byte) match {
+      case Unconsumed(bytes) => unconsumed(bytes)
+      case Proceeding(next) => Proceeding(new WrapperA(next))
+      case Done(msg) => Done(Left(msg))
+      case Failed(cause) => failed(cause)
+    }
+  }
+
+  private class WrapperB[A <: Message, B <: Message](delegate: MessageParser[B]) extends MessageParser[CompositeMessage[A, B]] {
+    def consume(byte: Byte): ParseState[CompositeMessage[A, B]] = delegate.consume(byte) match {
+      case Unconsumed(bytes) => unconsumed(bytes)
+      case Proceeding(next) => Proceeding(new WrapperB(next))
+      case Done(msg) => Done(Right(msg))
+      case Failed(cause) => failed(cause)
+    }
+  }
+
+  private def unconsumed[A <: Message, B <: Message](bytes: Seq[Byte]): ParseState[CompositeMessage[A, B]] = Unconsumed(bytes)
+
+  private def failed[A <: Message, B <: Message](cause: String): ParseState[CompositeMessage[A, B]] = Failed(cause)
+}
+
+class CompositeParser[A <: Message, B <: Message](a: MessageParser[A], b: MessageParser[B]) extends MessageParser[CompositeMessage[A, B]] {
+  import CompositeParser._
+
+  def consume(byte: Byte): ParseState[CompositeMessage[A, B]] = a.consume(byte) match {
+    case Unconsumed(_) => b.consume(byte) match {
+      case Unconsumed(bytes) => unconsumed(bytes)
+      case Proceeding(next) => Proceeding(new WrapperB(next))
+      case Done(msg) => Done(Right(msg))
+      case Failed(cause) => failed(cause)
+    }
+    case Proceeding(next) => Proceeding(new WrapperA(next))
+    case Done(msg) => Done(Left(msg))
+    case Failed(cause) => failed(cause)
+  }
+}
