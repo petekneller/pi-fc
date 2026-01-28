09b3af53049abfc79798082014b281c2c855dc86
Pete Kneller <mrkneller@hotmail.com>
2026-01-28 12:34:04 +0000

    Fixes failing ioctl test
    This was written to work on my old XPS 15 and the raspberry pi. And it
    certainly did at one point. But it doesn't work on either now.
    
    My current XPS because the temp file is created on a tmpfs and that
    doesn't support that ioctl op. The RPi because it seems to only
    support the 32-bit version of the op. Not surprising, but how did it
    used to work?
    
    Flipped the test to use an op that's supported on both tmpfs and the
    RPi's ext4. But still need to test for the bitness of the OS.
    The returned flags are all 0 on my dev machine and the RPi. Whether
    this holds in the future remains to be seen.

--- scala/ioctl/src/test/scala/ioctl/IOCtlTest.scala
+++ scala/ioctl/src/test/scala/ioctl/IOCtlTest.scala
@@ -2,27 +2,32 @@ package ioctl
 
 import org.scalatest.{FlatSpec, Matchers}
 import org.scalactic.TypeCheckedTripleEquals
-import com.sun.jna.{NativeLong, Native}
+import com.sun.jna.{NativeLong, Native, Platform}
 import java.io.File
 import java.nio.ByteBuffer
 import java.nio.ByteOrder.LITTLE_ENDIAN
 import syntax._
+import macros.IOR
 import IOCtl._
 
 class IOCtlTest extends FlatSpec with Matchers with TypeCheckedTripleEquals {
 
-  "ioctl" should "succeed with FS_IOC_GETVERSION on a valid file" in {
-    val tempfile = File.createTempFile("aaaa", "bbbb").getCanonicalPath
+  "ioctl" should "succeed with FS_IOC_GETFLAGS on a valid file" in {
+    val tempfile = File.createTempFile("aaaa", null, new File("target")).getCanonicalPath
     val fd = open(tempfile, O_RDONLY)
 
-    val FS_IOC_GETVERSION = 0x80087601L
+    // The standard/64bit version of this command is required on my dev laptop,
+    // the 32bit version is req'd on the RPi
+    val FS_IOC_GETFLAGS = IOR('f'.toByte, 1.toByte, 8) // 0x80086601 or 2148034049
+    val FS_IOC32_GETFLAGS = IOR('f'.toByte, 1.toByte, 4) // 0x80046601 or 2147771905
+    val op = if (Platform.is64Bit()) FS_IOC_GETFLAGS else FS_IOC32_GETFLAGS
 
-    val bb = ByteBuffer.allocate(Native.getNativeSize(classOf[Int]))
+    val bb = ByteBuffer.allocate(8)
     bb.order(LITTLE_ENDIAN)
-    ioctl(fd, new NativeLong(FS_IOC_GETVERSION), bb) should === (0)
+    ioctl(fd, new NativeLong(op), bb) should === (0)
     close(fd)
 
-    val version = bb.asIntBuffer.get.unsigned
+    val version = bb.asLongBuffer.get
     version should be >(0L)
   }
 
