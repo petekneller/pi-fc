9f7f0fb61fd3dc3cf5f12cae8b9197642cc48f73
Pete Kneller <mrkneller@hotmail.com>
2026-03-10 13:34:27 +0000

    Pushes `.toBytes` down into the trait
    It's now common to all messages since the checksums are handled properly.

--- scala/core/src/main/scala/core/device/gps/ublox/UbxMessage.scala
+++ scala/core/src/main/scala/core/device/gps/ublox/UbxMessage.scala
@@ -6,7 +6,14 @@ sealed trait UbxMessage extends Message {
   val clazz: Byte
   val id: Byte
   def payload: Seq[Byte]
-  def checksum: (Byte, Byte)
+
+  override def toBytes: Seq[Byte] = {
+    val Seq(length1, length2) = UbxMessage.payloadLength.toBytes(payload)
+    val checksum = UbxChecksum(clazz, id, payload)
+
+    import UbxParser.{ preamble1, preamble2 }
+    (preamble1 +: preamble2 +: clazz +: id +: length1 +: length2 +: payload) :+ checksum.ckA :+ checksum.ckB
+  }
 }
 
 object UbxMessage {
@@ -27,29 +34,16 @@ object UbxMessage {
 
 case class Unknown(clazz: Byte, id: Byte, payload: Seq[Byte]) extends UbxMessage {
   override def toString(): String = s"Unknown[UBX](class=$clazz, id=$id)"
-  def checksum: (Byte, Byte) = { val checksum = UbxChecksum(clazz, id, payload); (checksum.ckA, checksum.ckB) }
-
-  import UbxParser.{ preamble1, preamble2 }
-  def toBytes: Seq[Byte] = {
-    val Seq(length1, length2) = UbxMessage.payloadLength.toBytes(payload)
-    (preamble1 :: preamble2 :: clazz :: id :: length1 :: length2 :: payload.toList) :+ checksum._1 :+ checksum._2
-  }
 }
 
 case object RxBufferPoll extends UbxMessage {
   val clazz: Byte = 0x0A.toByte
   val id: Byte = 0x07.toByte
   def payload: Seq[Byte] = Seq.empty[Byte]
-  def checksum: (Byte, Byte) = (0x11.toByte, 0x3D.toByte)
-
-  def toBytes: Seq[Byte] = Unknown(clazz, id, Seq.empty[Byte]).toBytes
 }
 
 case object TxBufferPoll extends UbxMessage {
   val clazz: Byte = 0x0A.toByte
   val id: Byte = 0x08.toByte
   def payload: Seq[Byte] = Seq.empty[Byte]
-  def checksum: (Byte, Byte) = (0x12.toByte, 0x40.toByte)
-
-  def toBytes: Seq[Byte] = Unknown(clazz, id, Seq.empty[Byte]).toBytes
 }
